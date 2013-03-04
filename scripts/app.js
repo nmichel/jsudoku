809b63af689ab77ee96e6a7f30f08c4311a43beb
nicolas michel <nicolas.michel.lava@gmail.com>
2013-03-04 17:51:01 +0100

    Split solve() in sloveStart() and sloveStep().

--- scripts/app.js
+++ scripts/app.js
@@ -16,15 +16,39 @@ requirejs.config({
 
 require(["jquery", "app/sudoku", "app/sudoku_view", "app/sudoku_solver"], function($, Sudoku, SudokuView, SudokuSolver) {
     console.log("Sudoku loaded");
-    var sudokuGrid = new Sudoku(2, 2);
+    var sudokuGrid = new Sudoku(4, 4);
     var sudokuView = new SudokuView(sudokuGrid, "sudoku_container");
     sudokuGrid.addObserver(sudokuView);
 /*    sudokuGrid.setup(
         [[1, 0, 4],
          [2, 0, 3]]);*/
+
+/*
+    var rawSolve = function() {
+        var sudokuSolver = new SudokuSolver();
+        sudokuSolver.solveStart(sudokuGrid);
+        while (true) {
+            var cont = sudokuSolver.solveStep();
+            if (cont) {
+                continue; // <== 
+            }
+            break; // <== 
+        }
+    };
+*/
+
     $("#button_solve").click(function() {
         var sudokuSolver = new SudokuSolver();
-        sudokuSolver.solve(sudokuGrid);
+        sudokuSolver.solveStart(sudokuGrid, {shuffle: false});
+        var stepCount = 1;
+        var solveStep = function() {
+            var cont = sudokuSolver.solveStep();
+            if (cont) {
+                $("#step_count").html(stepCount++);
+                setTimeout(solveStep, 1);
+            }
+        };
+        setTimeout(solveStep, 1); // Boostrap
     });
 });
 
--- scripts/app/sudoku_solver.js
+++ scripts/app/sudoku_solver.js
@@ -1,65 +1,92 @@
+// Credit goes to http://sedition.com/perl/javascript-fy.html
+// 
+function fisherYates ( myArray ) {
+  var i = myArray.length, j, tempi, tempj;
+  if ( i == 0 ) return false;
+  while ( --i ) {
+     j = Math.floor( Math.random() * ( i + 1 ) );
+     tempi = myArray[i];
+     tempj = myArray[j];
+     myArray[i] = tempj;
+     myArray[j] = tempi;
+   }
+}
+
 define(
     ["app/sudoku"],
     function() {
         function SudokuSolver() {
-            this.sodoku = null;
+            this.model = null;
         }
 
         SudokuSolver.prototype = {};
         SudokuSolver.prototype.constructor = SudokuSolver;
 
-        SudokuSolver.prototype.solve = function(model) {
-            var cellCoords = model.getFreeCells(); // [[X0, Y0], [X1, Y1], ..., [Xn, Yn]]
-            if (cellCoords.length == 0) {
+        SudokuSolver.prototype.solveStart = function(model, options) {
+            var shuffle = (options && options.shuffle) || false; // Yes, I known, '|| false' should be omitted.
+
+            this.model = model;
+            this.stack = [];
+            this.currentCellId = 0;
+            this.cellCoords = model.getFreeCells(); // [[X0, Y0], [X1, Y1], ..., [Xn, Yn]]
+            if (this.cellCoords.length == 0) {
                 return; // <== Nothing to do because no free cells.
             }
 
-            var stack = [];
-            var currentCellId = 0;
+            if (shuffle) {
+                fisherYates(this.cellCoords);
+            }
 
-            var coords = cellCoords[currentCellId];
-            stack.push({
+            var coords = this.cellCoords[this.currentCellId];
+            this.stack.push({
                 cellCoords: coords,
-                cellFreeValues: model.getFreeValuesForCell(coords[0], coords[1]), // [V1, V2, ... Vn]
+                cellFreeValues: this.model.getFreeValuesForCell(coords[0], coords[1]), // [V1, V2, ... Vn]
                 idxNextValue: 0
             });
 
-            while (stack.length > 0) {
-                var cellDesc = stack[stack.length-1];
+        }
 
-                if (cellDesc.idxNextValue == cellDesc.cellFreeValues.length) {
-                    var posX = cellDesc.cellCoords[0];
-                    var posY = cellDesc.cellCoords[1];
-                    model.resetCell(posX, posY);
-                    stack.pop();
-                    --currentCellId;
-                    continue; // <== Current cell has no more free values to test.
-                }
+        SudokuSolver.prototype.solveStep = function() {
+            if (this.stack.length == 0) {
+                return false; // <== 
+            }
 
-                var nextValue = cellDesc.cellFreeValues[cellDesc.idxNextValue];
+            var cellDesc = this.stack[this.stack.length-1];
+
+            if (cellDesc.idxNextValue == cellDesc.cellFreeValues.length) {
                 var posX = cellDesc.cellCoords[0];
                 var posY = cellDesc.cellCoords[1];
-                ++cellDesc.idxNextValue;
-
-                var retValue = model.setCellValue(posX, posY, nextValue);
-                if (retValue == nextValue) {
-                    continue; // <== Value doesn't fit.
-                }
-
-                if (currentCellId == cellCoords.length-1) {
-                    break; // <== Found a fitting value for the last cell. Grid solved !
-                }
-
-                // Stack the next free cell
-                // 
-                ++currentCellId;
-                var coords = cellCoords[currentCellId];
-                stack.push({
-                    cellCoords: coords,
-                    cellFreeValues: model.getFreeValuesForCell(coords[0], coords[1]),
-                    idxNextValue: 0
-                });
+                this.model.resetCell(posX, posY);
+                this.stack.pop();
+                --this.currentCellId;
+                return true; // <== Current cell has no more free values to test.
             }
+
+            var nextValue = cellDesc.cellFreeValues[cellDesc.idxNextValue];
+            var posX = cellDesc.cellCoords[0];
+            var posY = cellDesc.cellCoords[1];
+            ++cellDesc.idxNextValue;
+
+            var retValue = this.model.setCellValue(posX, posY, nextValue);
+            if (retValue == nextValue) {
+                return true; // <== Value doesn't fit.
+            }
+
+            if (this.currentCellId == this.cellCoords.length-1) {
+                return false; // <== Found a fitting value for the last cell. Grid solved !
+            }
+
+            // Stack the next free cell
+            // 
+            ++this.currentCellId;
+            var coords = this.cellCoords[this.currentCellId];
+            this.stack.push({
+                cellCoords: coords,
+                cellFreeValues: this.model.getFreeValuesForCell(coords[0], coords[1]),
+                idxNextValue: 0
+            });
+
+            return true; // <== 
         };
 
 
